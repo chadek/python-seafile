7e41ebeb7b1f5a976802813ca7644781642eef6a
chadek <32199566+chadek@users.noreply.github.com>
2021-03-10 15:08:09 +0100

    Allow token use only
    This init function prevent the usage of token only if username/password isn't define, I suggest to make also make these arguments optional.

--- seafileapi/__init__.py
+++ seafileapi/__init__.py
@@ -1,6 +1,6 @@
 from seafileapi.client import SeafileApiClient
 
 
-def connect(server, username, password, token=None, verify_ssl=True):
+def connect(server, username=None, password=None, token=None, verify_ssl=True):
     client = SeafileApiClient(server, username, password, token, verify_ssl)
     return client
